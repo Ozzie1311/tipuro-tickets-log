1d08ad61f872d6b4ba320340e9d380ef94a83029
Oswaldo <rodriosw13@gmail.com>
2026-04-23 11:39:30 -0400

    Comenzando con el frontend. paginas, contexto y componentes

---
+++ client/src/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+//Agregando interceptor para no tener que enviar el token en cada petición. Con esto centralizamos el envio del token en cada petición.
+const api = axios.create({
+  baseUrl: 'http://localhost:3000/api',
+})
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
+
+export default api
