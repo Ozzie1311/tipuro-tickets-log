1d4ee405a664a2f07f63f5bde7a45bebf46a91ea
Oswaldo <rodriosw13@gmail.com>
2026-04-28 20:16:50 -0400

    intentando arreglar oneSignal

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import OneSignal from 'react-onesignal'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+OneSignal.init({
+  appId: 'efd951dd-54ad-4eab-aefe-bed564801cc1',
+  notifyButton: { enable: false },
+  allowLocalhostAsSecureOrigin: true,
+}).then(() => {
+  console.log('OneSignal iniciado')
+})
